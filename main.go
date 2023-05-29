3d79c9d8df54a03fe45d8420baaad185319a4910
Sequera86 <sequera.86@gmail.com>
2023-05-29 11:14:25 +0200

    funcion convierto a texto

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Sequera86/godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
 	variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
